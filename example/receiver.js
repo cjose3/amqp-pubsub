c7a07f97217f363814ba7748da83dd18c8fc2ddc
Carlos Marcano <cmarcano631@gmail.com>
2016-05-02 16:10:28 -0400

    update version

--- example/receiver.js
+++ example/receiver.js
@@ -8,22 +8,3 @@ console.log(queue)
 
 pubsub.connect(rx)
 pubsub.subscribe(queue, message => console.log('[+] %s', message.content.toString()))
-
-// var amqp = require('amqplib/callback_api');
-//
-// amqp.connect('amqp://localhost', function(err, conn) {
-//   conn.createChannel(function(err, ch) {
-//     var ex = 'logs';
-//
-//     ch.assertExchange(ex, 'fanout', {durable: false});
-//
-//     ch.assertQueue('', {exclusive: true}, function(err, q) {
-//       console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
-//       ch.bindQueue(q.queue, ex, '');
-//
-//       ch.consume(q.queue, function(msg) {
-//         console.log(" [x] %s", msg.content.toString());
-//       }, {noAck: true});
-//     });
-//   });
-// });
--- example/sender.js
+++ example/sender.js
@@ -17,18 +17,3 @@ pubsub.publish(queue, process.argv[2])
       process.exit(0)
     }, 500)
   })
-
-// var amqp = require('amqplib/callback_api');
-//
-// amqp.connect('amqp://localhost', function(err, conn) {
-//   conn.createChannel(function(err, ch) {
-//     var ex = 'logs';
-//     var msg = process.argv.slice(2).join(' ') || 'Hello World!';
-//
-//     ch.assertExchange(ex, 'fanout', {durable: false});
-//     ch.publish(ex, '', new Buffer(msg));
-//     console.log(" [x] Sent %s", msg);
-//   });
-//
-//   setTimeout(function() { conn.close(); process.exit(0) }, 500);
-// });
